3565b3f0b7bfdb6da8756ad1e2fae5d43cdb6196
Nicolas Oga <2112.oga@gmail.com>
2014-01-29 22:19:19 -0300

    Add SessionsController to manage google oauth2 callback
    [Finishes #64788284]

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
-  validates :name, :email, presence: true
+  validates :email, presence: true
   validates :email, uniqueness: true
   validates :email, format: { with: /\A[^@]+@[^@]+\..+\z/ }
 
-  def self.fetch_by_auth_hash(auth)
+  def self.fetch(auth)
     find_by_provider_and_uid(auth['provider'], auth['uid']) ||
     create_with_auth_hash(auth)
   end
@@ -12,8 +12,7 @@ class User < ActiveRecord::Base
     create!(
       provider: auth['provider'],
       uid:      auth['uid'],
-      email:    auth['info']['email'],
-      name:     auth['info']['name'],
+      email:    auth['info']['email']
     )
   end
 end
