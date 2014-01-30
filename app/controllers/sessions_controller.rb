eb4c3e044ae89753d66f2c514e0152a4fffd5120
Nicolas Oga <2112.oga@gmail.com>
2014-01-29 22:19:19 -0300

    Add SessionsController to manage google oauth2 callback
    [Finishes #64788284]

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,16 @@
 class SessionsController < ApplicationController
+  def create
+    user = User.fetch(auth_hash)
+    session[:user_id] = user.id
+    redirect_to root_path, notice: "Welcome back #{user.email}"
+  end
+
+  def destroy
+    reset_session
+    redirect_to root_path, notice: 'Logged out'
+  end
+
+  private
+
+  def auth_hash() env['omniauth.auth'] end
 end
